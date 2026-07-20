643f2fe488a2e0ccbdcc2ab768a9459d47dfc6d4
lauren-dot <laurenkimura23@gmail.com>
2026-07-20 11:13:57 -0700

    First Page

---
+++ src/components/hero/Speech.jsx
@@ -0,0 +1,28 @@
+
+import { TypeAnimation } from 'react-type-animation';
+
+{/*Text bubble*/}
+const Speech = () => {
+    return (
+        <div className='bubbleContainer'>
+            <div className="bubble">
+                <TypeAnimation
+                    sequence={[
+                        1000,
+                        'We produce food for Mice',
+                        1000, 
+                        'We produce food for Hamsters',
+                        1000,
+                    ]}
+                    wrapper="span"
+                    speed={40}
+                    deletionSpeed={60}
+                    repeat={Infinity}
+                />
+            </div>
+            <img src="/man.png" alt="" />
+        </div>
+    )
+}
+
+export default Speech;
